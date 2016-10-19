271a5e3c8911341f6e885ec2bccc5170a04ceae5
Kevin Hu <kevinhuwest@gmail.com>
2016-10-18 22:37:12 -0400

    abbreviations

--- allen_data/dev_human/normalized_genes/regions.py
+++ allen_data/dev_human/normalized_genes/regions.py
@@ -2,7 +2,7 @@ import csv
 from csv import DictReader
 
 with open("columns_metadata.csv") as gene_list:
-	regions = [row["structure_name"] for row in DictReader(gene_list)]
+	regions = [row["structure_name"] + " (" + row["structure_acronym"] + ")" for row in DictReader(gene_list)]
 #print a unique set of the regions
 regions = sorted(set(regions))
 for region in regions:
