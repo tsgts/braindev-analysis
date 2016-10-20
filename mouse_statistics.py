e5595a33c9d9bc4e396085b8daf1cd33dea74599
Kevin Hu <kevinhuwest@gmail.com>
2016-10-20 18:16:28 -0400

    pandas

--- mouse_statistics.py
+++ mouse_statistics.py
@@ -1,39 +1,22 @@
-import csv
-from csv import DictReader
-import urllib
-from urllib.request import urlretrieve
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import pandas as pd
 
-data = csv.reader(open("allen_data/dev_mouse/devmouse_histogram_values.csv"))
-col1 = next(data)
-regions = col1[6:]
+expression_data = pd.read_csv("allen_data/dev_mouse/devmouse_histogram_values.csv")
 
 def safe_log(x):
-	if x=='':
-		return 0
 	x = float(x)
 	if x<=0:
 		return 0
 	else:
    		return math.log10(x)
 
-region_expressions = {}
-
-def col_vals(col):
-	expression_data = open("allen_data/dev_mouse/devmouse_histogram_values.csv")
-	return [row[col] for row in DictReader(expression_data)]
-
-def log_float(vals):
-	return [safe_log(val) for val in vals]
-
-for region in regions:
-	print("Completed " + region)
-	region_expressions[region] = log_float(col_vals(region))
-	
-sns.distplot(region_expressions["RSP"], kde=False, rug=False)
+expression_data["RSP"] = expression_data["RSP"].apply(safe_log)
+expression_data["Tel"] = expression_data["Tel"].apply(safe_log)
 
 sns.set(color_codes=True)
+#sns.distplot(expression_data["RSP"], kde=False, rug=False)
+sns.regplot(x="RSP", y="Tel", data=expression_data)
 plt.show()
