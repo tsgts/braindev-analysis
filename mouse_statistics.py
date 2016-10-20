e0ebb50f6a88c300a553adadc0cf2d6832e272e9
Kevin Hu <kevinhuwest@gmail.com>
2016-10-20 18:49:55 -0400

    correlations

--- mouse_statistics.py
+++ mouse_statistics.py
@@ -4,19 +4,41 @@ import matplotlib.pyplot as plt
 import math
 import pandas as pd
 
+
 expression_data = pd.read_csv("allen_data/dev_mouse/devmouse_histogram_values.csv")
 
 def safe_log(x):
+	if math.isnan(x):
+		return 0
 	x = float(x)
 	if x<=0:
 		return 0
 	else:
    		return math.log10(x)
 
-expression_data["RSP"] = expression_data["RSP"].apply(safe_log)
-expression_data["Tel"] = expression_data["Tel"].apply(safe_log)
+regions = ["RSP","Tel","PHy","p3","p2","p1","M","PPH","PH","PMH","MH"]
 
-sns.set(color_codes=True)
-#sns.distplot(expression_data["RSP"], kde=False, rug=False)
-sns.regplot(x="RSP", y="Tel", data=expression_data)
-plt.show()
+for region in regions:
+	expression_data[region] = expression_data[region].apply(safe_log)
+
+#histograms for each region
+def histograms():
+	for region in regions:
+		print(region)
+		plt.figure(figsize=(60, 30))
+		sns.distplot(expression_data[region], kde=False, rug=False)
+
+		plt.savefig("figures/dev_mouse/histograms/"+region+".png")
+		plt.clf()
+
+#correlations between the regions
+def correlations():
+	for region1 in regions:
+		for region2 in regions:
+			print(region1 + "-" + region2)
+			plt.figure(figsize=(60, 30))
+			sns.regplot(x=region1, y=region2, data=expression_data)
+			plt.savefig("figures/dev_mouse/linregs/"+region1+"-"+region2+".png")
+			plt.clf()
+
+correlations()
