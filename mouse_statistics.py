d90eedc36b984ebed4aea9b778cae6ca3dc1c4b5
Kevin Hu <kevinhuwest@gmail.com>
2016-10-21 15:00:57 -0400

    changed statistics source

--- mouse_statistics.py
+++ mouse_statistics.py
@@ -5,22 +5,9 @@ import math
 import pandas as pd
 from scipy.stats import kendalltau
 
-expression_data = pd.read_csv("allen_data/dev_mouse/devmouse_histogram_values.csv")
-
-def safe_log(x):
-	if math.isnan(x):
-		return 0.0
-	x = float(x)
-	if x<=0:
-		return 0.0
-	else:
-   		return math.log10(x)
+expression_data = pd.read_csv("allen_data/dev_mouse/filtered_expression_values.csv")
 
 regions = ["RSP","Tel","PHy","p3","p2","p1","M","PPH","PH","PMH","MH"]
-
-#single log
-for region in regions:
-	expression_data[region] = expression_data[region].apply(safe_log)
 
 #histograms for each region
 def histograms():
