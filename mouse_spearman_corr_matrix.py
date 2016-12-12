c2b16cbd22d23138f690e63f76deb2df60917243
Kevin Hu <kevinhuwest@gmail.com>
2016-12-11 20:16:20 -0500

    fast spearman rho

--- mouse_spearman_corr_matrix.py
+++ mouse_spearman_corr_matrix.py
@@ -1,6 +1,17 @@
-from scipy.stats import spearmanr
 import json
 import pandas as pd
+
+def ranks(input):
+	seq = sorted(input)
+	index = [seq.index(v) for v in input]
+	return index
+
+def spearman_rho(a,b):
+	n = len(a)
+	a, b = ranks(a), ranks(b)
+	d_squareds = [(a[i]-b[i])**2 for i in range(0,n)]
+	df = n-2
+	return 1-6*sum(d_squareds)/(n*(n**2-1))
 
 with open('allen_data/dev_mouse/raw_dictionary_no_days.txt') as data_file:    
     data = json.load(data_file)
@@ -16,7 +27,7 @@ for i in genes:
 	print(i)
 	correlations = []
 	for j in genes:
-		correlations.append(spearmanr(data[i], data[j])[0]) 
+		correlations.append(spearman_rho(data[i], data[j])) 
 
 print(len(matrix))
 
@@ -25,3 +36,4 @@ print(df.shape)
 
 df.to_csv("allen_data/dev_mouse/mouse_corr_spearman_matrix.csv",sep=',', encoding='utf-8')
 
+print(spearman_rho(c,d))
