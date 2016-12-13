66376625365fa9d9f80253381452518b11811cec
Kevin Hu <kevinhuwest@gmail.com>
2016-12-13 08:31:44 -0500

    cluster colors

--- cluster.py
+++ cluster.py
@@ -3,6 +3,7 @@ import pylab
 import scipy.cluster.hierarchy as sch
 import json
 import numpy as np
+from collections import defaultdict
 
 with open('allen_data/dev_mouse/mouse_corr_pearson_matrix.txt') as data_file:    
     data = json.load(data_file)
@@ -17,7 +18,17 @@ fig = pylab.figure()
 axdendro = fig.add_axes([0.09,0.1,0.2,0.8])
 Y = sch.linkage(D, method='centroid')
 Z = sch.dendrogram(Y,no_labels=True,color_threshold=0.5*max(Y[:,2]),orientation="left")
-print(Z["leaves"])
+#print(Z["leaves"])
+print(Z["ivl"])
+
+cluster_idxs = defaultdict(list)
+for c, pi in zip(Z['color_list'], Z['icoord']):
+    for leg in pi[1:3]:
+        i = (leg - 5.0) / 10.0
+        if abs(i - int(i)) < 1e-5:
+            cluster_idxs[c].append(int(i))
+
+json.dump(cluster_idxs, open("allen_data/dev_mouse/dendrogram_colors.txt",'w'), indent=4)
 axdendro.set_xticks([])
 axdendro.set_yticks([])
 
