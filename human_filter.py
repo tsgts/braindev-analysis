bab9eb97481838af5349f2dcb0714d5117b9207d
Kevin Hu <kevinhuwest@gmail.com>
2016-12-27 12:04:51 -0500

    filtered human genes

---
+++ human_filter.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import json
+import collections
+
+with open('allen_data/dev_human/common_genes.txt') as data_file:    
+    good_genes = json.load(data_file)
+
+columns = [str(i) for i in range(0,525)]
+
+expression_matrix = pd.read_csv("allen_data/dev_human/normalized_genes/expression_matrix.csv",names=columns)
+
+rows_metadata = pd.read_csv("allen_data/dev_human/normalized_genes/rows_metadata.csv")
+filtered_genes = rows_metadata[rows_metadata["gene_symbol"].isin(good_genes)==True]
+
+
+#get rid of DTX2 duplicate
+filtered_genes = filtered_genes[filtered_genes["row_num"] != 44945]
+
+good_rows = list(filtered_genes["row_num"])
+
+expression_matrix=expression_matrix[expression_matrix["0"].isin(good_rows) == True]
+
+print(expression_matrix)
+
+expression_matrix.reset_index(inplace=True,drop=True)
+
+expression_matrix.to_csv("allen_data/dev_human/filtered_expression_values.csv",sep=',', encoding='utf-8')
--- human_mouse_compare.py
+++ human_mouse_compare.py
@@ -2,10 +2,11 @@ import numpy as np
 import math
 import pandas as pd
 from collections import Counter
+import json
 
-mouse_genes = pd.read_csv("allen_data/dev_mouse/log_expression_values.csv")
-mouse_genes = mouse_genes["gene_acronym"]
-mouse_genes = [x.upper() for x in mouse_genes]
+with open('allen_data/dev_mouse/list_of_genes.txt') as data_file:    
+    mouse_genes = json.load(data_file)
+mouse_genes = [x.upper() for x in list(set(mouse_genes))]
 
 human_genes = pd.read_csv("allen_data/dev_human/normalized_genes/rows_metadata.csv")
 human_genes = human_genes["gene_symbol"]
@@ -13,3 +14,5 @@ human_genes = human_genes["gene_symbol"]
 intersection = list(set(mouse_genes) & set(human_genes))
 
 print(len(intersection))
+
+json.dump(intersection,open('allen_data/dev_human/common_genes.txt','w'),indent=4)
